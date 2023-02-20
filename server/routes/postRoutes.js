0537731ab3d3b9b4e22a1ec02ec52d6e9fec4d2d
bojanciko <122114035+bojanciko@users.noreply.github.com>
2023-02-20 10:53:36 +0100

    first commit

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,43 @@
+import express from "express";
+import * as dotenv from 'dotenv'
+import uploadcare from 'uploadcare-widget'
+import { UploadClient } from '@uploadcare/upload-client'
+
+import Post from '../mongodb/models/post.js'
+
+dotenv.config()
+
+const router = express.Router()
+
+const client = new UploadClient({ publicKey: process.env.UPLOADCARE_PUBLIC_KEY })
+
+router.route('/').get(async (req, res) => {
+    try {
+      const posts = await Post.find({});
+      res.status(200).json({ success: true, data: posts });
+    } catch (err) {
+      res.status(500).json({ success: false, message: 'Fetching posts failed, please try again' });
+    }
+  });
+
+  router.route('/').post((req, res) => {
+    try {
+        const { name, prompt, photo } = req.body;
+        //client.uploadFile(photo).then((file) => {
+            const newPost = Post.create({
+                name,
+                prompt,
+                photo
+            });
+            //console.log(file.uuid)
+            
+            res.status(200).json({ success: true, data: newPost });
+        //}).catch((err) => console.log(err))
+        //console.log(prompt)
+            
+    } catch {
+        res.status(500).json({ success: false, message: 'Unable to create a post, please try again' });
+    }
+  })
+
+export default router
